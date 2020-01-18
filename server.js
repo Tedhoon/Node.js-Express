3c6b5031c30c33f36c33e33b20cd10702371ffa5
Tedhoon <gt0305@likelion.org>
2020-01-18 17:33:21 +0900

    router구성 및 server구동 js 수정

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var router = require('./router/main')(app);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
-})
+    console.log("http://127.0.0.1:3000")
+});
 
 // let http = require('http');
 // let fs = require('fs');
